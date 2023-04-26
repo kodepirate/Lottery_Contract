1514285a8f3c58d30cb473816e24115bc432f6ea
kodepirate <aniket.das9062@gmail.com>
2023-04-27 02:03:08 +0530

    added lotteryhistory, lotteryid

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -4,9 +4,22 @@ pragma solidity ^0.8.19;
 contract Lottery{
     address public owner; 
     address payable[] public  players;
-
+    uint public lotteryid;
+    mapping (uint => address payable ) public lotteryhistory;
+ 
     constructor(){
         owner = msg.sender;
+        lotteryid = 1;
+    }
+
+    function getBalance() public view returns (uint){
+        return  address(this).balance;
+    }
+    function getWinnerLottery(uint id) public  view returns (address payable ){
+        return lotteryhistory[id];
+    }
+    function getPlayers() public  view returns(address payable [] memory){
+        return  players;
     }
 
     function enter() public  payable  {
@@ -21,13 +34,25 @@ function getrandomnumber() public  view returns (uint){
     );
 }
 
-function pickWinner()public {
-    require(msg.sender  == owner);
+function pickWinner()public onlyOwner {
+
+    
    uint index = getrandomnumber() % players.length;
    players[index].transfer(address(this).balance);
 
+    lotteryhistory[lotteryid] = players[index];
+    
+   lotteryid++;
+
+  
+
    //reset the array
    players = new address payable [](0);
+}
+
+modifier onlyOwner(){
+    require(msg.sender  == owner);
+    _;
 }
 
 }
